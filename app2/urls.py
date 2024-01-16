d197a8543867aa5df5f19fcd4c20933fb26c641e
s1f10210201 <85723559+s1f10210201@users.noreply.github.com>
2024-01-16 19:16:49 +0900

    gfgvfc
    fd

--- app2/urls.py
+++ app2/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from accounts import views
 from app2 import views
-from .views import index, TrainingView, FAQView, FoodView, PlanView, chatapi
+from .views import index, TrainingView, FAQView, FoodView, PlanView
 
 # from .views import YourView
 
@@ -11,7 +11,6 @@ urlpatterns = [
     path('plan/', views.PlanView.as_view(), name='plan'),
     path('food/', views.FoodView.as_view(), name='food'),
     path('', views.index, name='index'),
-    path('chatapi/', chatapi, name='chatapi'),
     path('index/', index, name='index'),
     # path('api/v1/', YourView.as_view(), name='your_view_name')
 ]
